626c1e07e0495b0ad96b0b95789af3139e4f28fa
Guilherme Endres <guilherme.endres@arctouch.com>
2017-01-19 21:21:37 -0200

    Adds FeedViewController

---
+++ photographyForum/photographyForum/Modules/Feed/Views/FeedViewController.swift
@@ -0,0 +1,28 @@
+//
+// Created by Guilherme Endres on 19/01/17.
+// Copyright (c) 2017 HubChat. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FeedViewController: UIViewController {
+
+  // MARK: Views
+
+  // MARK: Variables
+
+  // MARK: Lifecycle
+
+  override func loadView() {
+    super.loadView()
+    view.backgroundColor = UIColor.blue
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+  // MARK: Actions
+}
